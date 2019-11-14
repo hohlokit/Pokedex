962eef94263f6668e879c2a88ed20a00805e806c
Mykola Shwayko <kolik2001@gmail.com>
2019-11-14 23:01:23 +0200

    Updates

---
+++ precache-manifest.e9772bbde0bd35745a6afbb6707acb51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e9f8cc002cc22389f0894694991354",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "dfb259d4eccc72450f4e",
+    "url": "/Pokedex/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "beb42e55a3278cac2468",
+    "url": "/Pokedex/static/css/main.2db6b2ff.chunk.css"
+  },
+  {
+    "revision": "dfb259d4eccc72450f4e",
+    "url": "/Pokedex/static/js/2.aab70a00.chunk.js"
+  },
+  {
+    "revision": "beb42e55a3278cac2468",
+    "url": "/Pokedex/static/js/main.942defaf.chunk.js"
+  },
+  {
+    "revision": "34dcb7c62138091398c4",
+    "url": "/Pokedex/static/js/runtime-main.ba04c60a.js"
+  }
+]);
